7e1d72e10adda155a513ef2c47e2648422e586af
Kevin <okenka6@gmail.com>
2026-09-25 15:24:49 +0200

    Tretia uloha

--- zadanie1a.sql
+++ zadanie1a.sql
@@ -42,3 +42,7 @@ SELECT o.order_id, c.customer_name, o.sales FROM orders o
 INNER JOIN customers c ON o.customer_id = c.customer_id
 WHERE o.sales > 500
 ORDER BY o.sales DESC;
+
+SELECT o.order_id, c.customer_name, p.category, o.sales FROM orders o
+INNER JOIN customers c ON o.customer_id = c.customer_id
+INNER JOIN products p ON o.product_id = p.product_id;
