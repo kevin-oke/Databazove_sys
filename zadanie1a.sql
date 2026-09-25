81de3cbaba87db117b55aeae47d66d37251fb87a
Kevin <okenka6@gmail.com>
2026-09-25 15:05:03 +0200

    Druha uloha

--- zadanie1a.sql
+++ zadanie1a.sql
@@ -37,3 +37,8 @@ SELECT * FROM customers;
 SELECT * FROM products;
 
 SELECT * FROM orders;
+
+SELECT o.order_id, c.customer_name, o.sales FROM orders o
+INNER JOIN customers c ON o.customer_id = c.customer_id
+WHERE o.sales > 500
+ORDER BY o.sales DESC;
